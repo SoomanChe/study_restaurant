5debfcbf28ee79074eb9e4c732a9faf188737bb3
Sooman Che <soomanche199@gmail.com>
2021-04-24 15:17:30 +0900

    project scaffold

---
+++ restaurant-api/src/test/java/com/sooman_che/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sooman_che.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
